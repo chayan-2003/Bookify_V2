f7048f378b5d0714606238333789416aa6fe6713
Chayan Ghosh <113135687+chayan-2003@users.noreply.github.com>
2025-05-03 04:03:38 +0530

    Update Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -11,27 +11,28 @@ const Home = () => {
   return (
     <div className=" ">
       {/* Navbar and Header Section */}
-      <div className="pb-10">
+      <div className=" container mx-auto pb-10">
         <Navbar />
         <Header />
-      </div>
+    
 
       {/* Main Content Container */}
-      <div className="container mx-auto px-6 lg:px-20 py-8">
-        <h1 className="homeTitle text-3xl font-extrabold mb-6 text-gray-800">Explore Featured Locations</h1>
+      
+        <h1 className="homeTitle text-3xl font-extrabold mb-6 text-gray-800 mt-10">Explore Featured Locations</h1>
         <Featured />
 
     
 
         <h1 className="homeTitle mt-10 text-3xl font-extrabold text-gray-800">Homes Guests Love</h1>
         <FeaturedProperties />
-        <div className="w-100">
+       
         <MailList />
-        </div>
+ 
 
         <Footer />
       </div>
     </div>
+  
   );
 };
 
