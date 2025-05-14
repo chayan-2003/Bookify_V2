3760072de89d16869ca26130f42d136b24cf1540
Chayan Ghosh <113135687+chayan-2003@users.noreply.github.com>
2025-05-14 22:33:58 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -46,7 +46,7 @@ mongoose.connection.on("disconnected", () => {
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://bookify-tawny.vercel.app'],
+  origin: ['http://localhost:3000', 'https://bookify-tawny.vercel.app',"https://bookify-v2-alpha.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
